338cf871a9934ad7fe55335c09dacb3f69d44f05
Naraa01 <naraam0910@gmail.com>
2024-02-25 15:13:49 +0800

    add react app & nextjs app

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# react-and-tailwind
-##
+# mono repo
+
+    1. nest-app
+    2. nextjs-app
+    3. react-app
+
+### Describe projects
+
+#### nest-app
+
+    TypeScript project
+    - qwe
+    - rty
+
+    Ерөнхий
---
+++ react-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-red-300">
+        <p>Hello world</p>
+
+        <p>FOoter</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
